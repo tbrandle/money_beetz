8b01c73cbb3fa4934359b29002a0226fcda45d06
Tim <tbrandle53@gmail.com>
2017-05-23 16:36:06 -0600

    working timer function.  Only console logging at this point

---
+++ javascripts/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Timer from './Timer';
+
+
+export default class App extends Component {
+  constructor(){
+    super()
+
+  }
+
+  render(){
+    return (
+      <div>Working
+        <Timer />
+      </div>
+    )
+  }
+
+}
