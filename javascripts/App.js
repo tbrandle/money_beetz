8bceca14eb81f46faa3aea37b2c22d25581fa73b
Jon Willemain <jon.willemain@gmail.com>
2017-05-24 11:50:08 -0600

    Moved the Beetz component from App to ChoiceWrapper

--- javascripts/App.js
+++ javascripts/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Timer from './Timer';
-import Beetz from './Beetz.js';
 
 
 export default class App extends Component {
@@ -13,7 +12,6 @@ export default class App extends Component {
     return (
       <div className="app-title">Money Beetz
         <Timer />
-        <Beetz />
       </div>
     )
   }
