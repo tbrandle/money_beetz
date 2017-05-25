31aa49e77259585c6ddad8cfb9d584095528716e
Staci McWilliams <stacinicole86@gmail.com>
2017-05-24 19:55:45 -0600

    Trim audio file

--- songs/songArray.js
+++ songs/songArray.js
@@ -1,19 +1,19 @@
-  // const audio1 = new Audio("../songs/loser.mp3")
-  const audio2 = new Audio("../songs/faith.mp3")
-  // const audio3 = new Audio("../songs/Empire.mp3")
-  // const audio4 = new Audio("../songs/inOneEar.mp3")
-  // const audio5 = new Audio("../songs/mia.mp3")
-  // const audio6 = new Audio("../songs/NeverGoingToGiveYouUp.mp3")
-  // const audio7 = new Audio("../songs/hip-hop.mp3")
-  // const audio8 = new Audio("../songs/noFlexZone.mp3")
+const audio1 = new Audio("../songs/loser.mp3");
+const audio2 = new Audio("../songs/faith_trim.mov");
+const audio3 = new Audio("../songs/Empire.mp3");
+const audio4 = new Audio("../songs/inOneEar.mp3");
+const audio5 = new Audio("../songs/mia.mp3");
+const audio6 = new Audio("../songs/NeverGoingToGiveYouUp.mp3");
+const audio7 = new Audio("../songs/hip-hop.mp3");
+const audio8 = new Audio("../songs/noFlexZone.mp3");
 
 module.exports = [
-  // audio1,
-  audio2
-  // audio3,
-  // audio4,
-  // audio5,
-  // audio6,
-  // audio7,
-  // audio8
-]
+  audio1,
+  audio2,
+  audio3,
+  audio4,
+  audio5,
+  audio6,
+  audio7,
+  audio8
+];
